ddf277eb8a5b9e47d69021648e38ef14fe339544
Minh Hùng <86279989+Ming-Hungf@users.noreply.github.com>
2023-03-08 15:16:51 +0700

    Add files via upload

---
+++ Bai6_2/cau1.dart
@@ -0,0 +1,10 @@
+enum Gender{
+  MALE,
+  FEMALE,
+  OTHERS
+}
+void main(){
+  for(var item in Gender.values){
+    print(item);
+  }
+}
---
+++ bai6_extra/ex2.dart
@@ -0,0 +1,15 @@
+class Mother{
+  void display(){
+    print("Mother");
+  }
+}
+class Daughter extends Mother{
+  @override
+  void display(){
+    print("object");
+  }
+}
+void main(){
+  var dau = new Daughter();
+  dau.display();
+}
